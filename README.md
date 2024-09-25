f585c52ab00a09013568664bdd36d4aab395018d
Sammie05 <snweke98@gmail.com>
2024-09-25 15:42:44 +0100

    updates

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Buildyourportfolio
-team project by kamo and samuel
+team project by kamo and samuel 
+
+In this projects were building an E-commerce platform called "Localluxe" for Buying and selling of luxury clothes. 
---
+++ scripts.js
@@ -0,0 +1,4 @@
+// Simple JavaScript Example for toggling cart visibility
+document.querySelector('.cart-button').addEventListener('click', function() {
+    document.querySelector('.cart-modal').classList.toggle('visible');
+});
